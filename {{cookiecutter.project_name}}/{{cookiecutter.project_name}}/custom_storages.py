60b5e8d71fd04163675c1d7579cc75bc682efcc0
Chris Barry <chris.barry@makemoredigital.com>
2016-07-19 12:06:07 +0100

    added s3 bucket name to setup script

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/custom_storages.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/custom_storages.py
@@ -5,8 +5,8 @@ from storages.backends.s3boto import S3BotoStorage
 
 class StaticStorage(S3BotoStorage):
     location = settings.STATICFILES_LOCATION
-    bucket_name = "{{cookiecutter.s3_bucket_name}}"
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
 
 class MediaStorage(S3BotoStorage):
     location = settings.MEDIAFILES_LOCATION
-    bucket_name = "{{cookiecutter.s3_bucket_name}}"
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
